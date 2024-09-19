f72c48ec7841fa7ba3e353f81b8dfa56cc5c72c5
omkar kakade <kakadeom16@gmail.com>
2024-09-19 23:33:05 +0530

    chinese food added

--- src/Home/Chinese/ChinRecipe.jsx
+++ src/Home/Chinese/ChinRecipe.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import './IndRecipe.css';
+import './ChinRecipe.css';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase'; // Adjust path if necessary
 import { Link } from 'react-router-dom'
 
-export default function IndRecipe({ setEd }) {
+export default function ChinRecipe({ setEd }) {
   const [dishes, setDishes] = useState([]);
 
   const getRecipe = (foodId) => {
@@ -14,7 +14,7 @@ export default function IndRecipe({ setEd }) {
   useEffect(() => {
     const fetchDishes = async () => {
       try {
-        const collectionRef = collection(db, 'food');
+        const collectionRef = collection(db, 'food1');
         const querySnapshot = await getDocs(collectionRef);
         const dishesData = querySnapshot.docs.map(doc => doc.data());
         setDishes(dishesData);
@@ -30,22 +30,22 @@ export default function IndRecipe({ setEd }) {
   return (
     <div className="recipe-container">
       {dishes
-        .filter(food => food.Id < 10) // Filter to only include items with Id < 10
-        .map((food, index) => (
+        .filter(food1 => food1.Id < 10) // Filter to only include items with Id < 10
+        .map((food1, index) => (
           <div key={index} className="recipe-card">
             <div className="image-container">
-              <img src={food.imageURL} alt="Dish" />
+              <img src={food1.imageURL} alt="Dish" />
             </div>
             <div className="content">
               <h2 className="recipe-title">
-                {food.Id}.{food.name}
-                <img src={food.type} alt="Additional" className="additional-image" />
+                {food1.Id}.{food1.name}
+                <img src={food1.type} alt="Additional" className="additional-image" />
               </h2>
-              <p className="recipe-about">{food.About}</p>
+              <p className="recipe-about">{food1.About}</p>
             </div>
             <div className="button-container">
-              <Link to={`/InGet/${food.Id}`}>
-                <button className="get-recipe-button" onClick={() => getRecipe(food.Id)}>Get Recipe</button>
+              <Link to={`/ChGet/${food1.Id}`}>
+                <button className="get-recipe-button" onClick={() => getRecipe(food1.Id)}>Get Recipe</button>
               </Link>
             </div>
           </div>
